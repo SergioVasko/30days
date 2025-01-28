0e10465d1188dff7cf1e750d6f66ab21b35bccf1
Serhii Vasko <s.vasko@chargebacks911.com>
2025-01-28 13:46:09 -0500

    2

---
+++ resources/views/contact.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Page</title>
+</head>
+<body>
+Contact Page
+</body>
+</html>
+
+<?php
