c0d0f4420b9aac127122db62c5a1a2e2683f1d50
Serhii Vasko <s.vasko@chargebacks911.com>
2025-02-16 00:48:46 -0500

    30 done

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Random\RandomException;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employer>
@@ -14,12 +15,13 @@ class EmployerFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @throws RandomException
      */
     public function definition(): array
     {
         return [
             'name' => fake()->company(),
-            'logo' => fake()->imageUrl(),
+            'logo' => 'http://picsum.photos/seed/'.random_int(0, 100000).'/90',
             'user_id' => User::factory(),
         ];
     }
